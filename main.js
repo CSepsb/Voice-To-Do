ef364c2345d9e038bcac3bf203d876fc8147fae1
CSepsb <124709628+CSepsb@users.noreply.github.com>
2024-01-15 14:42:12 -0700

    done

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Voice To-Do
+// Voice To-Do(A to-do list website that talks to you)
 
 // Variables
 let num = 0;
@@ -11,26 +11,12 @@ let tasks = [];
 
 // Button Event Listener
 document.getElementById("btn").addEventListener("click", btnClicked);
+// List Select Event Listener
+document.getElementById("user").addEventListener("change", displayTasks);
 
-document.getElementById("user").addEventListener("change", function () {
-  checkUserData();
-  displayTasks();
-});
+// To-Do-List Basic Functions
 
-// To-Do-List functions
-function btnClicked() {
-  let menu = document.getElementById("menu").value;
-  if (menu === "add") {
-    add();
-  } else if (menu === "edit") {
-    edit();
-  } else if (menu === "remove") {
-    remove();
-  } else if (menu === "move") {
-    move();
-  }
-}
-
+// Add Task
 function add() {
   let item = prompt("Enter item:");
   if (item.length === 0) {
@@ -45,21 +31,22 @@ function add() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }
 }
-
+// Remove Task
 function remove() {
   let index = prompt("Position to remove:");
+  let user = document.getElementById("user").value;
   if (isValidIndex(index)) {
-    let removedTask = tasks.splice(index - 1, 1)[0];
+    tasks.splice(index - 1, 1)[0];
     outputEl.innerHTML = "";
     displayTasks();
-    speakRemove(index, removedTask.user);
+    speakRemove(index, user);
     num--;
   } else {
     speakError();
   }
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-
+// Edit Task
 function edit() {
   let index = prompt("Enter position:");
   if (isValidIndex(index)) {
@@ -77,7 +64,7 @@ function edit() {
   }
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-
+// Move Task
 function move() {
   let index1 = prompt("Move item from:");
   let index2 = prompt("Move item to:");
@@ -93,17 +80,32 @@ function move() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+// Select and Force the right Function to run
+function btnClicked() {
+  let menu = document.getElementById("menu").value;
+  if (menu === "add") {
+    add();
+  } else if (menu === "edit") {
+    edit();
+  } else if (menu === "remove") {
+    remove();
+  } else if (menu === "move") {
+    move();
+  }
+}
+
 // Key Event Listener Push-To-Talk
 document.addEventListener("keydown", keyDownHandler);
 document.addEventListener("keyup", keyUpHandler);
 
+// On
 function keyDownHandler(event) {
   if (event.code === "Space") {
     spacePressed = true;
     pushToSpeak();
   }
 }
-
+// Off
 function keyUpHandler(event) {
   if (event.code === "Space") {
     spacePressed = false;
@@ -113,6 +115,8 @@ function keyUpHandler(event) {
 }
 
 // Talking functions
+
+// Lists All Task and Changes the Icon and Title color
 function pushToSpeak() {
   if (spacePressed) {
     document.getElementById("img").src = "img/unmute.png";
@@ -125,7 +129,7 @@ function pushToSpeak() {
     document.querySelector("h1").style.color = "red";
   }
 }
-
+// Lists All Tasks
 function speakAll() {
   if (tasks.length === 0) {
     let message = new SpeechSynthesisUtterance(
@@ -143,23 +147,23 @@ function speakAll() {
     }
   }
 }
-
+// Lists Added Task and User
 function speakAdd(taskNum, taskText, user) {
   let message = new SpeechSynthesisUtterance(
-    `${taskText} added as task number ${taskNum} `
+    `${taskText} added as task number ${taskNum} in ${user} list`
   );
   window.speechSynthesis.speak(message);
   console.log(message);
 }
-
+// Lists Removed Task and User
 function speakRemove(taskNum, user) {
   let message = new SpeechSynthesisUtterance(
-    `Task number ${taskNum} removed from ${user} list`
+    `Task number ${taskNum} removed in ${user} list`
   );
   window.speechSynthesis.speak(message);
   console.log(message);
 }
-
+// Lists Edited Task and User
 function speakEdit(taskNum, originalTask, newTask, user) {
   let message = new SpeechSynthesisUtterance(
     `Task number ${taskNum}, "${originalTask}" has been replaced by "${newTask}" in ${user} list`
@@ -167,7 +171,7 @@ function speakEdit(taskNum, originalTask, newTask, user) {
   window.speechSynthesis.speak(message);
   console.log(message);
 }
-
+// Lists Moved Task and User
 function speakMove(taskNum1, taskNum2, user) {
   let message = new SpeechSynthesisUtterance(
     `Task number ${taskNum1}, has been moved in the place of task number ${taskNum2} in ${user} list`
@@ -175,49 +179,30 @@ function speakMove(taskNum1, taskNum2, user) {
   window.speechSynthesis.speak(message);
   console.log(message);
 }
-
+// Informs User of an Error
 function speakError() {
   let message = new SpeechSynthesisUtterance(`Error`);
   window.speechSynthesis.speak(message);
   console.log(message);
 }
-
-function isValidIndex(index) {
-  return !isNaN(index) && index >= 1 && index <= tasks.length;
-}
-
+// Stops Speach
 function stopSpeak() {
   window.speechSynthesis.cancel();
 }
 
+// Validates index
+function isValidIndex(index) {
+  return !isNaN(index) && index >= 1 && index <= tasks.length;
+}
+
 // Persistant data
-// Check if tasks exist
 if (localStorage.getItem("tasks")) {
   tasks = JSON.parse(localStorage.getItem("tasks"));
 
-  if (!Array.isArray(tasks)) {
-    tasks = [];
-  }
-
-  checkUserData();
   displayTasks();
 }
 
-// Array user function
-function checkUserData() {
-  let user = document.getElementById("user").value;
-
-  if (user === "day") {
-    num = tasks.filter((task) => task.user === user).length;
-  } else if (user === "week") {
-    num = tasks.filter((task) => task.user === user).length;
-  } else if (user === "month") {
-    num = tasks.filter((task) => task.user === user).length;
-  } else if (user === "year") {
-    num = tasks.filter((task) => task.user === user).length;
-  }
-}
-
+// Displays Tasks From Specific List 
 function displayTasks() {
   outputEl.innerHTML = "";
 
